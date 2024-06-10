c46b654e6cfc9c695c5671badf517caa70fa9dac
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-06-10 12:52:57 +0200

    (Terminé) job 07

---
+++ jour01/job07/Addition.java
@@ -0,0 +1,10 @@
+package jour01.job07;
+public class Addition {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 73;
+
+        float quotient = (float) num1 / num2;
+        System.out.println("Résultat de la division : " + quotient);
+    }
+}
---
+++ jour01/job08/Facture.java
@@ -0,0 +1,17 @@
+package jour01.job08;
+
+public class Facture {
+    public static void main(String[] args) {
+
+        float prix = (float) 49.99;
+        int quantite = 3;
+        float tarifHT = prix * quantite;
+        float tva = (float) 0.2;
+
+        float tarifTTC = (float) tarifHT + tarifHT * tva;
+
+        System.out.println("Le montant total HT est de " + tarifHT);
+        System.out.println("Le montant de la taxe est de " + (tarifTTC - tarifHT));
+        System.out.println("Le montant total TTC est de " + tarifTTC);
+    }
+}
