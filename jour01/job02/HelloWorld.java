d5dc329b73926171e9e7d563e514c238011fc233
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-06-10 12:31:03 +0200

    (Terminé) job 04

--- jour01/job02/HelloWorld.java
+++ jour01/job02/HelloWorld.java
@@ -1,4 +1,4 @@
-package jour01;
+package jour01.job02;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
---
+++ jour01/job04/DeclarationVariables.java
@@ -0,0 +1,6 @@
+package jour01.job04;
+public class DeclarationVariables {
+    public static void main(String[] args) {
+        int a = 0, b = 0, c = 0;
+    }
+}
