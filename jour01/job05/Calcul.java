d7606c7c3b72697ce35220199d4e471ec0b21005
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-06-10 12:33:10 +0200

    (Terminé) job 05

---
+++ jour01/job05/Calcul.java
@@ -0,0 +1,13 @@
+package jour01.job05;
+
+public class Calcul {
+    public static void main(String[] args) {
+        int a = 7;
+        System.out.println(a);
+        a = a + 3;
+        System.out.println(a);
+        a = a * 5;
+        System.out.println(a);
+    }
+}
+
