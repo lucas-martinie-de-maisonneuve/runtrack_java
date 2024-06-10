d6cd87108ba0dfecbcdbb2e91982de1e5a16a235
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-06-10 11:52:16 +0200

    (Terminé) job 03

---
+++ jour01/job03/HelloWorld.java
@@ -0,0 +1,14 @@
+package jour01.job03;
+
+// Commentaire sur une ligne : méthode main
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
+/*
+Commentaire 
+sur 
+plusieurs 
+lignes 
+*/
