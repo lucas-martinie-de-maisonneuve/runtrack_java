f4b72c0692069e9250b9d14a2e2c12e704a48d24
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-06-10 21:47:52 +0200

    (Modifié) Job10

--- jour01/job10/NomPrenom.java
+++ jour01/job10/NomPrenom.java
@@ -1,14 +1,25 @@
 package jour01.job10;
 
+import java.util.Scanner;
 import java.util.StringJoiner;
 
 public class NomPrenom {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Entrez votre prénom: ");
+        String prenom = scanner.nextLine();
+
+        System.out.print("Entrez votre nom: ");
+        String nom = scanner.nextLine();
+
         StringJoiner stringJoiner = new StringJoiner(" ", " ", " ");
-        stringJoiner.add("Lucas");
-        stringJoiner.add("Martinie");
+        stringJoiner.add(prenom);
+        stringJoiner.add(nom);
 
         String concat = stringJoiner.toString();
-        System.out.println(concat);
+        System.out.println("Résultat: " + concat);
+
+        scanner.close();
     }
 }
