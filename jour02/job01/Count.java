8d4e92770f665aa26d5f591f6cefd14b75f11059
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-06-11 09:36:42 +0200

    (terminé) jour2.job1 - boucle qui affiche les nombres de 1 à 100

---
+++ jour02/job01/Count.java
@@ -0,0 +1,11 @@
+package jour02.job01;
+
+public class Count {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        }while(i <= 100);
+    }
+}
