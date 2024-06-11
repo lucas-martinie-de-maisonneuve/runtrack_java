ac3a9d91eb1bb5239e496e7355040973e9395529
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-06-11 09:37:56 +0200

    (terminé) jour2.job2 - boucle qui affiche les nombres de 100 à 1

---
+++ jour02/job02/ReverseCount.java
@@ -0,0 +1,11 @@
+package jour02.job02;
+
+public class ReverseCount {
+    public static void main(String[] args) {
+        int i = 100;
+        do {
+            System.out.println(i);
+            i--;
+        }while(i >= 1);
+    }
+}
