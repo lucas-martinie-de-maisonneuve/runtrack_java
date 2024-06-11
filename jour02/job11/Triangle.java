db64820b4e06b9194c03eb536a53aa4aa1df13e3
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-06-11 10:50:59 +0200

    (terminé) jour02.job11 - construire un triangle avec une hauteur donné

---
+++ jour02/job11/Triangle.java
@@ -0,0 +1,10 @@
+package jour02.job11;
+
+public class Triangle {
+    public static void main(String[] args) {
+        String aster = "*" ;
+        for(int i = 1; i <=10; i++){
+            System.out.println(aster.repeat(i));
+        }
+    }
+}
