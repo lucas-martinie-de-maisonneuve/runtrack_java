0a4af65554f3d1cd442fffd280525e0f357fdb84
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-06-17 09:29:12 +0200

    (terminé) jour03/job02 - Modifier valeur tableau et afficher en console

---
+++ jour03/job02/Tableau.java
@@ -0,0 +1,23 @@
+package jour03.job02;
+
+public class Tableau {
+        public static void main(String[] args) {
+            int[] monTableau = {12, 6, 76, 89};
+    
+            System.out.println("Tableau avant modification :");
+            for (int i = 0; i < monTableau.length; i++) {
+                System.out.println("i" + i + " : " + monTableau[i]);
+            }
+    
+            monTableau[0] = 5;
+            monTableau[1] = 15;
+            monTableau[2] = 25;
+            monTableau[3] = 35;
+    
+            System.out.println("Tableau modifié :");
+            for (int i = 0; i < monTableau.length; i++) {
+                System.out.println("i" + i + " : " + monTableau[i]);
+            }
+        }
+    
+}
